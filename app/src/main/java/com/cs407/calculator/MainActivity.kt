d2fb4e5ec8588eb54101d051402bc766542fd043
ceoneill3 <ceoneill3@wisc.edu>
2024-09-19 11:29:53 -0500

    Final Commit

--- app/src/main/java/com/cs407/calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/calculator/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cs407.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,82 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val text1 = findViewById<EditText>(R.id.editTextText)
+        val text2 = findViewById<EditText>(R.id.editTextText3)
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        addButton.setOnClickListener {
+            val input1 = text1.text.toString().toInt()
+            val input2 = text2.text.toString().toInt()
+
+            val total = input1 + input2
+            val result = "Result: $total"
+
+            val intent = Intent(this, CalcDisplay::class.java)
+
+            intent.putExtra("EXTRA_MESSAGE", result)
+
+            startActivity(intent)
+
+
+        }
+
+        val subButton = findViewById<Button>(R.id.subButton)
+        subButton.setOnClickListener {
+            val input1 = text1.text.toString().toInt()
+            val input2 = text2.text.toString().toInt()
+
+            val total = input1 - input2
+            val result = "Result: $total"
+
+            val intent = Intent(this, CalcDisplay::class.java)
+
+            intent.putExtra("EXTRA_MESSAGE", result)
+
+            startActivity(intent)
+
+
+        }
+
+        val multButton = findViewById<Button>(R.id.multButton)
+        multButton.setOnClickListener {
+            val input1 = text1.text.toString().toInt()
+            val input2 = text2.text.toString().toInt()
+
+            val total = input1 * input2
+            val result = "Result: $total"
+
+            val intent = Intent(this, CalcDisplay::class.java)
+
+            intent.putExtra("EXTRA_MESSAGE", result)
+
+            startActivity(intent)
+
+
+        }
+
+        val divButton = findViewById<Button>(R.id.divideButton)
+        divButton.setOnClickListener {
+            try {
+                val input1 = text1.text.toString().toInt()
+                val input2 = text2.text.toString().toInt()
+
+                val total = input1 / input2
+                val result = "Result: $total"
+
+                val intent = Intent(this, CalcDisplay::class.java)
+
+                intent.putExtra("EXTRA_MESSAGE", result)
+
+                startActivity(intent)
+            } catch(e: ArithmeticException){
+                Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
     }
 }
